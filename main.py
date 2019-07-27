25aff416e4406a8ffbafa0c99e09f1e6015066ac
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-07-27 17:05:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
 from guizero import App
 
-app = App()
+class colors:
+    class workspace:
+        px1 = "#aaaaaa"
+        px2 = "#ffffff"
+    class schemes:
+        red     = "#ff0000"
+        green   = "#00ff00"
+        blue    = "#0000ff"
+        yellow  = "#ffff00"
 
+class graphics:
+    class color_red:
+        pass
+    class color_orange:
+        pass
+    class color_yellow:
+        pass
+    class color_green:
+        pass
+    class color_blue:
+        pass
+    class color_purple:
+        pass
+    class color_brown:
+        pass
+    class color_black:
+        pass
+    class color_white:
+        pass
+
+
+
+
+app = App(title="drawing app")
+#
+##
+#
+app.display()
