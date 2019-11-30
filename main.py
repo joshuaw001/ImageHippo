ed27996ad2101d1564a96d6a30cbe46b8af5226b
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-11-29 20:32:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
 from guizero import App
-
+##############
+#COLORS class#
+##############
 class colors:
     class workspace:
         px1 = "#aaaaaa"
         px2 = "#ffffff"
     class schemes:
+        
         red     = "#ff0000"
         green   = "#00ff00"
         blue    = "#0000ff"
         yellow  = "#ffff00"
+    class palette:
+        fg = "#0000FF"
+        bg = "FFFFF00"
 
-class graphics:
-    class color_red:
-        bg = ["fire","abstract"]
-    class color_orange:
-        bg = ["fire"]
-    class color_yellow:
-        bg = ["fire"]
-    class color_green:
-        bg = ["abstract"]
-    class color_blue:
-        bg = ["abstract"]
-    class color_purple:
-        g = [""]
-    class color_brown:
-        g = [""]
-    class color_black:
-        g = [""]
-    class color_white:
-        g = [""]
-    class color_teal:
-        g = ["abstract"]
-    class color_pink:
-        g = [""]
-pp = App(title="drawing app")
+app = App(title="drawing app")
 ##
 #
 ##
